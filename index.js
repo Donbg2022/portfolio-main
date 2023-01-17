7b9ffa37dd10f82c5931a23b2fcbd36bde750a1f
Donovan <donovangriffin04@gmail.com>
2023-01-17 08:49:53 -0800

    styled responsive navbar

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ responsiveMenuBtn.addEventListener('click', () => {
   responsiveMenuBtn.classList.toggle('open')
   navBarLinks.classList.toggle('open')
 })
+
+navBarLinks.addEventListener('click', () => {
+  navBarLinks.classList.remove('open')
+  responsiveMenuBtn.classList.remove('open')
+
+
+})
