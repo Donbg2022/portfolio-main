e2e21899a8fe555db5fa3d5a96734d4199f7feb7
Donovan <donovangriffin04@gmail.com>
2023-02-17 17:11:46 -0800

    added nav toggle to change background color onclick

--- modules/MenuButton.js
+++ modules/MenuButton.js
@@ -3,15 +3,24 @@
 
 const responsiveMenuBtn = document.querySelector('.responsive-menu-btn')
 const navBarLinks = document.querySelector('.nav-links')
+const nav = document.querySelector('nav')
 
 const toggleMenuOpen = () => {
   responsiveMenuBtn.classList.toggle('open')
   navBarLinks.classList.toggle('open')
+  nav.classList.toggle('links-bg-color')
+
+  
 }
 
 const toggleMenuRemove = () => {
   navBarLinks.classList.remove('open')
   responsiveMenuBtn.classList.remove('open')
+  nav.classList.toggle('links-bg-color')
+  
+
 }
+
+
 
 export {toggleMenuOpen, toggleMenuRemove, responsiveMenuBtn, navBarLinks}
